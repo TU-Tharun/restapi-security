81bd3358fb1771482c6581dcbd9859da9d6010bd
TU-Tharun <123296854+TU-Tharun@users.noreply.github.com>
2023-01-22 15:24:21 +0530

    Add files via upload

---
+++ springsecurity/src/main/java/com/doc/SpringsecurityApplication.java
@@ -0,0 +1,24 @@
+package com.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class SpringsecurityApplication {
+	@GetMapping ("/demo")
+	private String demo() {
+		return "hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+				}
+		
+	}
+
+
